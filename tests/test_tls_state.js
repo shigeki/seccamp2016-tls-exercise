c52fe19009070505872b7246a8a65b0d2d694168
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2016-08-01 21:30:34 +0900

    fix7

--- tests/test_tls_state.js
+++ tests/test_tls_state.js
@@ -5,22 +5,29 @@ const TLS = require('../lib/tls.js').TLS;
 const Sample1 = require('./sample_data.js').Sample;
 const Sample2 = require('./sample_data2.js').Sample;
 const Connection = require('../lib/connection.js').Connection;
+const Dummy = require('./dummy_create_frame.js');
+function addDummy(connection) {
+  connection.createServerHello = Dummy.createServerHello;
+  connection.createCertificate = Dummy.createCertificate;
+  connection.createServerKeyExchange = Dummy.createServerKeyExchange;
+  connection.createServerHelloDone = Dummy.createServerHelloDone;
+  connection.createChangeCipherSpec = Dummy.createChangeCipherSpec;
+  connection.createServerFinished = Dummy.createServerFinished;
+}
 
 function Test(Sample) {
   describe('TLS Server State', function() {
     it('TLS_ST_BEFORE=>TLS_ST_OK', function() {
       var connection = new Connection(true);
+      addDummy(connection);
       connection.on('frameError', function(err) {
-        console.log('hoho', err);
       });
       connection.on('frame', function(frame, type) {
-        console.log(type, frame);
         if (type === 'ServerHelloDone') {
           connection.state.read(Sample.ClientKeyExchange);
         }
       });
-      console.log('Listerners', connection.listeners('frameError'));
-//      var ret = connection.state.read(Sample.ClientHello);
+      var ret = connection.state.read(Sample.ClientHello);
     });
   });
 }
