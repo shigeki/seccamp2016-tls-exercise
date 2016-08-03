de2da93e0b702d3340c8d17dd43c9614971c0f38
Shigeki Ohtsu <ohtsu@iij.ad.jp>
2016-08-03 13:25:59 +0900

    wip19

--- tests/test_connection.js
+++ tests/test_connection.js
@@ -23,10 +23,13 @@ function Test(Sample) {
                              Sample.ClientEncryptedApplicationData[1]
                             ];
       var connection = new Connection(true, true);
-      connection.client_write_key = Sample.ClientWriteKey;
-      connection.server_write_key = Sample.ServerWriteKey;
-      connection.client_write_iv = Sample.ClientWriteIV;
-      connection.server_write_iv = Sample.ServerWriteIV;
+      connection.key_block = {
+        client_write_key: Sample.ClientWriteKey,
+        server_write_key: Sample.ServerWriteKey,
+        client_write_iv: Sample.ClientWriteIV,
+        server_write_iv: Sample.ServerWriteIV
+      };
+      connection.pre_master_secret = Sample.PreMasterSecret;
       connection.on('error', function(e) {
         console.log(e);
       });
@@ -66,10 +69,13 @@ function Test(Sample) {
                              Sample.ServerEncryptedApplicationData[0]
                             ];
       var connection = new Connection(false, true);
-      connection.client_write_key = Sample.ClientWriteKey;
-      connection.server_write_key = Sample.ServerWriteKey;
-      connection.client_write_iv = Sample.ClientWriteIV;
-      connection.server_write_iv = Sample.ServerWriteIV;
+      connection.key_block = {
+        client_write_key: Sample.ClientWriteKey,
+        server_write_key: Sample.ServerWriteKey,
+        client_write_iv: Sample.ClientWriteIV,
+        server_write_iv: Sample.ServerWriteIV
+      };
+      connection.pre_master_secret = Sample.PreMasterSecret;
       connection.on('error', function(e) {
         console.log(e);
       });
@@ -94,22 +100,24 @@ function Test(Sample) {
     });
   });
   describe('ClientTest', function() {
-    var client_connection = new Connection(false);
-    var client_hello_obj = client_connection.frame_creator.createClientHello(client_connection);
-    var client_hello_buf = TLS.Handshake.ClientHello.encode(client_hello_obj);
-    var server_connection = new Connection(true);
-    server_connection.setCertificates(require('../lib/stub_cert_data.js').Certificates);
-    server_connection.read(client_hello_buf, function(e, conn) {
-      var client_key_exchange_obj = client_connection.frame_creator.createClientKeyExchange(conn);
-      var client_key_exchange_buf = TLS.Handshake.ClientKeyExchange.encode(client_key_exchange_obj);
-      server_connection.read(client_key_exchange_buf, function(e, conn) {
-        var change_cipher_spec_buf = TLS.ChangeCipherSpec.encode();
-        server_connection.read(change_cipher_spec_buf, function(e, conn) {
-          var client_finished_obj = client_connection.frame_creator.createClientFinished(client_connection);
-//          var client_finished_buf = TLS.Handshake.Finished.encode(client_finished_obj);
-//          server_connection.read(change_finished_buf, function(e) {
-//            console.log('DONE');
-//          });
+    it('ClientTest', function() {
+      var client_connection = new Connection(false, true);
+      var client_hello_obj = client_connection.frame_creator.createClientHello(client_connection);
+      var client_hello_buf = TLS.Handshake.ClientHello.encode(client_hello_obj);
+      var server_connection = new Connection(true);
+      server_connection.setCertificates(require('../lib/stub_cert_data.js').Certificates);
+      server_connection.read(client_hello_buf, function(e, conn) {
+        var client_key_exchange_obj = client_connection.frame_creator.createClientKeyExchange(conn);
+        var client_key_exchange_buf = TLS.Handshake.ClientKeyExchange.encode(client_key_exchange_obj);
+        server_connection.read(client_key_exchange_buf, function(e, conn) {
+          var change_cipher_spec_buf = TLS.ChangeCipherSpec.encode();
+          server_connection.read(change_cipher_spec_buf, function(e, conn) {
+            var client_finished_obj = client_connection.frame_creator.createClientFinished(conn);
+            var client_finished_buf = TLS.Handshake.Finished.encode(client_finished_obj, conn.cipher, conn.key_block.client_write_key, conn.key_block.client_write_iv);
+            server_connection.read(client_finished_buf, function(e) {
+              console.log('DONE', conn.state.constructor.name);
+            });
+          });
         });
       });
     });
